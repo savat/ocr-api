6b0bbf87a1848224be631afdf3f42a3a0584a377
savat <savat1619@gmail.com>
2026-04-14 05:41:14 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,28 +1,31 @@
 from fastapi import FastAPI, UploadFile, File
 from fastapi.responses import JSONResponse
-import pytesseract
+from paddleocr import PaddleOCR
 from PIL import Image
+import numpy as np
 import io
-import re
 
 app = FastAPI()
 
+
+ocr = PaddleOCR(lang='th', use_angle_cls=True, show_log=False)
+
 @app.get("/")
 def home():
-    return {"message": "OCR API is running! Use POST /ocr to upload an image."}
+    return {"message": "PaddleOCR API is running! Use POST /ocr to upload an image."}
 
 @app.post("/ocr")
 async def ocr_image(file: UploadFile = File(...)):
     contents = await file.read()
     img = Image.open(io.BytesIO(contents))
+    img_np = np.array(img)
     
+    result = ocr.ocr(img_np)
     
-    img = img.resize((int(img.width*1.5), int(img.height*1.5)), Image.LANCZOS)
+    if result and result[0]:
+        texts = [line[1][0] for line in result[0]]
+        full_text = '\n'.join(texts)
+    else:
+        full_text = ""
     
-    
-    text = pytesseract.image_to_string(img, lang='tha+eng')
-    
-    
-    cleaned_text = re.sub(r'\s+', ' ', text).strip()
-    
-    return JSONResponse(content={"text": cleaned_text})
+    return JSONResponse(content={"text": full_text})
